5fd61502720a5b1e210b76fcfb80499686ff0e64
Omar Farooq <omarf4c@gmail.com>
2022-06-04 17:02:20 +0500

    Routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoginComponent } from 'src/login/login.component';
+import { NavbarComponent } from 'src/navbar/navbar.component';
+import { SignupComponent } from 'src/signup/signup.component';
+import { RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
